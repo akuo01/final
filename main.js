7a1b69e0fef342e4d9027222f7f530485a35c164
akuo01 <akuo01@risd.edu>
2020-02-05 13:05:58 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,47 @@
+function changecolor1() {
+    id1.style.color = "#a8adf5";
+    var img1 = document.querySelector(".img1");
+    img1.style.display = "block";
+}
+
+function revertcolor1() {
+    id1.style.color = "white";
+    var img11 = document.querySelector(".img1");
+    img11.style.display = "none";
+}
+
+function changecolor2() {
+    id2.style.color = "#a8adf5";
+    var img2 = document.querySelector(".img2");
+    img2.style.display = "block";
+}
+
+function revertcolor2() {
+    id2.style.color = "white";
+    var img22 = document.querySelector(".img2");
+    img22.style.display = "none";
+}
+
+function changecolor3() {
+    id3.style.color = "#a8adf5";
+    var img3 = document.querySelector(".img3");
+    img3.style.display = "block";
+}
+
+function revertcolor3() {
+    id3.style.color = "white";
+    var img33 = document.querySelector(".img3");
+    img33.style.display = "none";
+}
+
+function changecolor4() {
+    id4.style.color = "#a8adf5";
+    var img4 = document.querySelector(".img4");
+    img4.style.display = "block";
+}
+
+function revertcolor4() {
+    id4.style.color = "white";
+    var img44 = document.querySelector(".img4");
+    img44.style.display = "none";
+}
